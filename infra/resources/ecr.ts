16c908390a0dc816d4e402b07021655c96b7ca5c
Sam Kitonyi <sam.kitonyi@gmail.com>
2021-07-07 20:28:12 +0300

    Set maximum number of untagged images in ECR

--- infra/resources/ecr.ts
+++ infra/resources/ecr.ts
@@ -1,14 +1,21 @@
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-
-import { getStack } from "@pulumi/pulumi";
 import { Config } from "@pulumi/pulumi";
 
-const stackName = getStack();
 const config = new Config();
 const appName = config.require("app-name");
+const maxImages = config.requireNumber("ecr-max-images");
 
-const repository = new awsx.ecr.Repository(appName);
+const repository = new awsx.ecr.Repository(appName, {
+  lifeCyclePolicyArgs: {
+    rules: [
+      {
+        selection: "untagged",
+        maximumNumberOfImages: maxImages,
+      },
+    ],
+  },
+});
+
 const image = repository.buildAndPushImage({
   dockerfile: "../docker/Dockerfile",
   context: "..",
